89173fbab4f0366dddd5b0ec4745ad576fd06635
tuyo1009@gmail.com <tuyo1009@gmail.com>
2014-01-26 09:52:28 +0900

    second rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,4 +12,18 @@ describe "StaticPages" do
       page.should have_content('Sample App')
     end
   end
+
+  describe "Help page" do
+  	it "should have the content 'Help'" do
+  		visit '/static_pages/help'
+  		page.should have_content('Help')
+  	end
+  end
+
+  describe "About Us" do
+  	it "should have the content'About Us'" do
+  		visit '/static_pages/about'
+  		page.should have_content('About Us')
+  	end
+  end
 end
